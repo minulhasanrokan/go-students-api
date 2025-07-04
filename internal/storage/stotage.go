051d0b114ca6f6411a1dd38f42a2136bcb37f33e
Md. Minul Hasan <minulhasanrokan@gmail.com>
2025-07-05 03:27:39 +0600

    final

--- internal/storage/stotage.go
+++ internal/storage/stotage.go
@@ -5,4 +5,5 @@ import "github.com/minulhasanrokan/students-api/internal/types"
 type Storage interface {
 	CreateStudent(name string, email string, age int64) (int64, error)
 	GetStudentById(id int64) (types.Student, error)
+	GetStudents() ([]types.Student, error)
 }
